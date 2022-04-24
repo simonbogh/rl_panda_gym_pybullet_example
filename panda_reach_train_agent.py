850611b559cc64efa13df10a2b02044cac0e63a7
Simon Bøgh <sb@m-tech.aau.dk>
2022-04-24 15:22:05 +0200

    Added train PPO agent script

---
+++ panda_reach_train_agent.py
@@ -0,0 +1,56 @@
+import gym
+import panda_gym
+import torch as th
+from stable_baselines3 import PPO
+from stable_baselines3.common.env_util import make_vec_env
+
+"""
+Train PPO agent in the PandaReach-v1 environment.
+
+We use a dense reward function, which makes it easier to learn a policy than with a sparse reward function. A dense reward function can also be used.
+
+Reward functions:
+    distance_threshold = 0.05
+    d = distance(achieved_goal, desired_goal)  # Norm distance (Euclidean distance)
+
+    Sparse: -(d > distance_threshold).astype(np.float32)
+    Dense:  -d
+
+"""
+#############################
+# Create environment
+env = gym.make("PandaReach-v1", render=True, reward_type="dense")
+
+#############################
+# Set up policy and train agent
+
+# Custom actor (pi) and value function (vf) networks
+# of two layers of size 64 each with Relu activation function
+policy_kwargs = dict(
+    activation_fn=th.nn.ReLU, net_arch=[dict(pi=[64, 64], vf=[64, 64])]
+)
+
+#############################
+# Set up PPO model
+# https://stable-baselines3.readthedocs.io/en/master/modules/ppo.html#stable_baselines3.ppo.MultiInputPolicy
+model = PPO(
+    "MultiInputPolicy",
+    env,
+    verbose=1,
+    tensorboard_log="runs",
+    batch_size=64,
+    normalize_advantage=True,
+    learning_rate=0.001,
+    policy_kwargs=policy_kwargs,
+)
+
+#############################
+# Train agent
+model.learn(
+    total_timesteps=100000,
+    tb_log_name="PPO_lr0.001_hu64_64_bs64",
+)
+
+#############################
+# Save trained model
+model.save("PandaReach_PPO_v1_model")
